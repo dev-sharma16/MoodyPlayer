19002f903e420a66c475aa6d873512b02683df59
Dev Sharma <devsharma1683@gmail.com>
2025-07-22 11:59:20 +0530

    saving the song object in db and route for fetching a song based on mood

--- Backend/src/routes/song.route.js
+++ Backend/src/routes/song.route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const multer = require("multer");
 const uploadFile = require("../service/storage.service");
+const songModel = require("../models/song.model")
 
 const router = express.Router();
 const upload = multer({storage: multer.memoryStorage()})
@@ -8,14 +9,35 @@ const upload = multer({storage: multer.memoryStorage()})
 router.post("/songs", upload.single("audio"), async(req,res) => {
     console.log(req.body);
     console.log(req.file);
-    
+
     const fileData  = await uploadFile(req.file)
     console.log("Uploaded file data : ",fileData);
+
+    const song = await songModel.create({
+        title: req.body.title,
+        artist: req.body.artist,
+        mood: req.body.mood,
+        audio: fileData.url,
+        audioId: fileData.fileId,
+    })
     
     res.status(201).json({
         message: "Song created successfully",
-        song: req.body,
+        saved_song: song,
     });
 });
+
+router.get("/songs", async(req,res) => {
+    const {mood} = req.query;
+
+    const songs = await songModel.find({
+        mood: mood
+    })
+
+    res.status(200).json({
+        message: "Songs fetched successfully",
+        songs: songs
+    })
+})
 
 module.exports = router; 
